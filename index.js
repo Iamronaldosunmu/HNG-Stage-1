c28897ef8e877163cef1dfa12f66c6046baca246
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-10-28 20:51:58 +0100

    Added cors to the project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 4000;
 
 // Start listening for requests on the server
